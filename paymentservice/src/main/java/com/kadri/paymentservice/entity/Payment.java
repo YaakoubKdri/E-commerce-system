73aa6e9922621a21d22d02733b6683b143ce247b
kadri <kadriyaakoub@gmail.com>
2025-12-30 20:15:04 -0600

    Payment Service: add entity

---
+++ paymentservice/src/main/java/com/kadri/paymentservice/entity/Payment.java
@@ -0,0 +1,28 @@
+package com.kadri.paymentservice.entity;
+
+import com.kadri.common.enums.PaymentStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Entity
+@Table(name = "payment")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long orderId;
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus status;
+    private Instant createdAt;
+}
